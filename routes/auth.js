87225f62cbd6d81ce7c09deff9cb51f1f951462b
hamdiazi <hamdiazi00@gmail.com>
2026-05-01 03:26:49 +0700

    membuat file Auth.js berbeda untuk logic, route review

--- routes/auth.js
+++ routes/auth.js
@@ -1,36 +1,19 @@
 const express = require ('express');
 const router = express.Router();
 const User = require ('../models/user');
+const AuthController = require('../controllers/auth'); //memanggil controller > auth.js
 const wrapAsync = require('../utils/wrapAsync');
 const passport = require('passport');
 
 // route untuk ke form register 
-router.get('/register', (req, res) => {
-    res.render('auth/register');
-})
+router.get('/register', AuthController.registerform)
 
 // router post agar bisa create user di form register
-router.post('/register', wrapAsync(async (req,res) => {
-    // res.send(req.body);
-    try {
-        const {email, username, password} = req.body;
-        const user = new User ({email, username});
-        const registerUser = await User.register(user, password);
-        req.login(registerUser, (err) => {
-            if(err) return next(err);
-        req.flash('success_msg', 'You are registered  and logged in');
-        res.redirect('/places');
-        })
-    } catch (error) {     
-        req.flash('error_msg', error.message);
-        res.redirect('/register');
-    }
-}))
+router.post('/register', wrapAsync(AuthController.registerUser))
 
 // route untuk ke form login 
-router.get('/login', (req, res) => {
-    res.render('auth/login');
-})
+router.get('/login', AuthController.loginForm)
+
 // fungsi untuk login 
 router.post('/login', passport.authenticate('local',{
     failureRedirect:'/login',
@@ -38,18 +21,9 @@ router.post('/login', passport.authenticate('local',{
         type: 'error_msg',
         msg:'Invalid username or password'
     }
-}), (req, res) => {
-    // console.log(req.user);
-    req.flash('success_msg','You are logged in');
-    res.redirect('/places');
-})
+}), AuthController.login)
+
 // fungsi untuk log out 
-router.post('/logout', (req, res) => {
-    req.logout(function(err) {
-        if(err) {return next(err)}
-        req.flash('success_msg', 'You are Logged out');
-        res.redirect('/login');
-    })
-})
+router.post('/logout', AuthController.logout)
 
 module.exports = router
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Place = require('../models/place');   // import models mongoose dari file place.js
 const Review = require('../models/review');// import model review
+const ReviewControllers = require('../controllers/reviews'); //memanggil atau import file controllers > review.js
 const { reviewSchema } = require('../schemas/review'); // schemas untuk reviews
 const ErrorHandler = require('../utils/ErrorHandler');   //require untuk handling error dari class ExpressError
 const wrapAsync = require('../utils/wrapAsync')  //requeire untuk handle error
@@ -22,28 +23,10 @@ const validateReview = (req, res, next) => {
 }
 
 // Restfull untuk simpan review
-router.post('/', isAuth, isValidObjectId('/places'), validateReview, wrapAsync (async (req, res) => {
-    const { place_id } = req.params;
-
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    await review.save();
-
-    const place = await Place.findById(place_id);
-    place.reviews.push(review);
-    await place.save(); 
-    
-    req.flash('success_msg','Review added successfully')
-    res.redirect(`/places/${place_id}`);
-}))
+router.post('/', isAuth, isValidObjectId('/places'), validateReview, wrapAsync (ReviewControllers.store))
 
 // restfull hapus review
-router.delete('/:review_id', isAuth,isAuthorReview, isValidObjectId('/places'), wrapAsync(async (req, res) => {
-    const { place_id, review_id} = req.params;
-    await Place.findByIdAndUpdate(place_id, {$pull: { reviews : review_id } } );
-    await Review.findByIdAndDelete(review_id);
-    req.flash('success_msg','Review deleted successfully')
-    res.redirect(`/places/${place_id}`);
-}) )
+router.delete('/:review_id', isAuth,isAuthorReview, isValidObjectId('/places'), 
+wrapAsync(ReviewControllers.destroy))
 
 module.exports = router;
